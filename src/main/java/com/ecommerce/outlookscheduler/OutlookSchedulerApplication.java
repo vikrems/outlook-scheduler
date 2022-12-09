407f4e996246e000e0d8c3a37ece167baf4f4f4b
vikshe <vikrem.sheker@hidglobal.com>
2022-12-09 07:13:20 +0530

    first commit

---
+++ src/main/java/com/ecommerce/outlookscheduler/OutlookSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.outlookscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlookSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlookSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/outlookscheduler/OutlookSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.outlookscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlookSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
